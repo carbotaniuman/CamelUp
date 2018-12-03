66488d64a23dadf29a963c7b6db5e1d0ac09428b
Boxcat21 <42621953+Boxcat21@users.noreply.github.com>
2018-12-02 21:59:58 -0600

    handDrawer

--- src/main/java/drawers/HandDrawer.java
+++ src/main/java/drawers/HandDrawer.java
@@ -1,13 +1,38 @@
 package drawers;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Shape;
 
 import gamestate.*;
 
 public class HandDrawer 
 {
-	public void drawHand(Graphics g, Player p)
+	//size of screen: 1920x1080
+	//size of hand: 1920x280
+	public void drawHand(Graphics g, Player p) 
 	{
-		
+		Color[] colors = {Color.WHITE, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE};
+		//WholeHand
+		g.setColor(Color.ORANGE);
+		g.fillRect(0, 800, 1920, 280);
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 800, 1920, 280);
+		//DesertCard
+		g.setColor(Color.YELLOW);
+		g.fillRect(0, 800, 180, 100);
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 800, 180, 100);
+		//RaceBettingCards
+		for(int i = 0; i < 5; i++)
+		{
+			g.setColor(Color.CYAN);
+			g.fillRect(i*150, 900, 150, 180);
+			g.setColor(Color.BLACK);
+			g.drawRect(i*150, 900, 150, 180);
+			g.setColor(colors[i]);
+			g.drawString(p.getName(), i*150+75, 990);
+		}
+		//
 	}
 }
