ec10ac582b3c11d38eb5681e39c74898a6569c86
Boxcat21 <42621953+Boxcat21@users.noreply.github.com>
2018-12-09 21:12:34 -0600

    space fix

--- src/main/java/graphics/GraphicsPanel.java
+++ src/main/java/graphics/GraphicsPanel.java
@@ -2,10 +2,6 @@ package graphics;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
@@ -29,7 +25,8 @@ public class GraphicsPanel extends JPanel {
 		TrackDrawer.drawTrack(g, gamestate.getTrack(), gamestate.getCurPlayer());
 	}
 	@Override
-	public Dimension getPreferredSize() {
+	public Dimension getPreferredSize() 
+	{
 		return new Dimension(1920, 1080);
 	}
 }
